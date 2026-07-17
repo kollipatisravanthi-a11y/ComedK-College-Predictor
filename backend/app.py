a23136ceec77315326d2f3a27529ee94e24a475f
Sravanthi Kollipati <kollipatisravanthi@gmail.com>
2026-07-17 20:11:38 +0530

    prep: configure for pythonanywhere and vercel deployment

--- backend/app.py
+++ backend/app.py
@@ -25,4 +25,4 @@ from backend.routes import *
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000))  # Use Render's default port if not set
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run(host="0.0.0.0", port=port, debug=True)
